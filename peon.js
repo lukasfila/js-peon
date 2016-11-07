d3cb038b8c13b8f19cb210be4c2c254d026a3528
lukasfila <fila.lukas@gmail.com>
2016-11-07 15:09:40 +0100

    Peon js fix.

--- peon.js
+++ peon.js
@@ -1,9 +1,15 @@
 /* globals require, process, console, __dirname, process*/
 var peon = require("./node_modules/peon/peon/index"),
+	buildOnly = process.argv[2] === "b",
+	testsEnabled = process.argv[2] === "t",
 	path = __dirname,
 	port;
 
 
+if (buildOnly) {
+	peon.build(path);
+} else {
 //noinspection JSUnresolvedVariable,Eslint
-port = process.env.PORT || 3333;
-peon.start(path, port);
+	port = process.env.PORT || 3333;
+	peon.start(path, port, testsEnabled);
+}
